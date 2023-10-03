40d6a27134f368a53c6d6e695db8dcf517d7a323
sapthaa <andre.saptha@hotmail.com>
2023-10-03 10:08:49 +0200

    Interface - Abstract

---
+++ src/com/Andre/Interface/Abstract/Animal.java
@@ -0,0 +1,23 @@
+package com.Andre.Interface.Abstract;
+
+
+// Abstract - inheritance ( arv )
+public interface Animal {
+
+    // TODO - Give relevant names
+    // TODO - Implement inheritance
+    
+
+    // Abstract method
+    void myAbstractMethod();
+
+
+
+
+
+    // Default
+    default void test(){}
+
+
+
+}
